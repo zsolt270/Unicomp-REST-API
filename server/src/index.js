9af16b20942c8e7137909d7f3039ebf152bd9413
zsolt270 <zsolt270000@gmail.com>
2024-12-07 17:57:32 +0100

    Add db connection to index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/dbConnection.js";
 
 dotenv.config();
+connectDB();
 const app = express();
 const PORT = process.env.PORT || 3003;
 
