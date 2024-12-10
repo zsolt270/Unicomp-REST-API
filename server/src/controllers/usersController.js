8ec3a7a9bb3a06fcbf39fcc99f5c1800e49368de
zsolt270 <zsolt270000@gmail.com>
2024-12-10 16:26:39 +0100

    Finish signup endpoint

--- server/src/controllers/usersController.js
+++ server/src/controllers/usersController.js
@@ -5,6 +5,26 @@ export default class User {
   async signUp(req, res) {
     console.log(req.body);
 
-    res.json(req.body);
+    const salt = await bcrypt.genSalt();
+    const securedPwd = await bcrypt.hash(req.body.password, salt);
+
+    if (!securedPwd) {
+      res.status(500);
+      throw new Error("Something went wrong!");
+    }
+
+    const user = await Users.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: securedPwd,
+      reviews: [],
+    });
+
+    if (!user) {
+      res.status(500);
+      throw new Error("Something went wrong!");
+    }
+
+    res.status(201).json(user);
   }
 }
--- server/src/middleware/requestValidator.js
+++ server/src/middleware/requestValidator.js
@@ -3,7 +3,6 @@ import { validationResult } from "express-validator";
 export const requestValidator = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    console.log(errors.array());
     res.status(400);
     throw new Error(errors.array()[0].msg);
   }
